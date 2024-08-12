ed9dc757d84a118dc4d625d53cf03e6fe3cd29cd
Sebastian Duarte Quijada <a223205136@unison.mx>
2024-08-11 23:25:37 -0700

    codigo final

--- Correlacion.java
+++ Correlacion.java
@@ -3,7 +3,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class Main {
+public class Correlacion {
     public static void main(String[] args) {
         String archivo = "startup-profit.csv";
         String linea;
